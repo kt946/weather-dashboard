558946c38a6cc8790788e23174a3decd38fc3528
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-06-10 22:41:09 -0400

    add get weather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 // unique api key
 var apiKey = "&appid=64c2d8fc8d96271e3ba05c28616d91c9";
 var searchHistory = [];
-// variables for city latitude and longitude coordinates 
-var lan = "";
-var lon = "";
 
-// first get searched city's lat and lon fron weather api
+// pass coordinates from 'getCityLocation()' into one call api
+var getCityWeather = function(lat, lon) {
+    // formate the OpenWeather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?&lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly" + apiKey;
+    // make a request to the url
+    fetch(apiUrl)
+        .then(function(response) {
+            //console.log(response);
+            // request successful
+            if (response.ok) {
+                response.json().then(function(data) {
+                    console.log(data);
+                })
+            }
+        })
+};
+
+// first get searched city's latitude and longitude fron weather api
 var getCityLocation = function(city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
     fetch(apiUrl)
@@ -14,11 +28,13 @@ var getCityLocation = function(city) {
             // request successful
             if (response.ok) {
                 response.json().then(function(data) {
-                    console.log(data);
-                    lan = data.coord.lat;
-                    lon = data.coord.lon;
-                    console.log(lan);
-                    console.log(lon);
+                    //console.log(data);
+                    // get coordinates then pass into weather function
+                    var y = data.coord.lat;
+                    var x = data.coord.lon;
+                    console.log(y);
+                    console.log(x);
+                    getCityWeather(y, x);
                 })
             } else {
                 alert("Error: City does not exist");
@@ -30,26 +46,5 @@ var getCityLocation = function(city) {
 };
 
 var searchTerm = "London";
-
-/*var getCity = function(city) {
-    // formate the OpenWeather api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?&appid=64c2d8fc8d96271e3ba05c28616d91c9";
-    // make a request to the url
-    fetch(apiUrl)
-        .then(function(response) {
-            console.log(response);
-            // request successful
-            if (response.ok) {
-                response.json().then(function(data) {
-                    //console.log(data);
-                })
-            } else {
-                alert("Error: City does not exist");
-            }
-        })
-        .catch(function(error) {
-            alert("Unable to connect to OpenWeather");
-        });
-};*/
 
 getCityLocation(searchTerm);
